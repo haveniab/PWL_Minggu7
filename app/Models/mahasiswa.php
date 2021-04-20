620baca1c6dc0942a70251fde5b597668f5afb9c
haveniab <havenia09@gmail.com>
2021-04-20 22:12:54 +0700

    mengisi kode mahasiswa.php

--- app/Models/mahasiswa.php
+++ app/Models/mahasiswa.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
-class mahasiswa extends Model
+class mahasiswa extends Model //Definisi Model
 {
-    use HasFactory;
-}
+    protected $table="mahasiswa"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    public $timestamps= false; 
+    protected $primaryKey = 'Nim'; // Memanggil isi DB Dengan primarykey
+
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+        ];
+};
